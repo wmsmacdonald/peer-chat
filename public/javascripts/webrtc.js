5ec35c2fda85de64d4db48ff61006890d85f6b25
William MacDonald <wmsmacdonald@gmail.com>
2015-12-19 01:59:56 -0600

    remove getusermedia

--- public/javascripts/webrtc.js
+++ public/javascripts/webrtc.js
@@ -12,8 +12,6 @@ window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || 
 window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
 
 function pageReady() {
-    localVideo = document.getElementById('localVideo');
-    remoteVideo = document.getElementById('remoteVideo');
 
     serverConnection = new WebSocket('ws://127.0.0.1:3434');
     serverConnection.onmessage = gotMessageFromServer;
@@ -23,16 +21,6 @@ function pageReady() {
         audio: false
     };
 
-    if(navigator.getUserMedia) {
-        navigator.getUserMedia(constraints, getUserMediaSuccess, errorHandler);
-    } else {
-        alert('Your browser does not support getUserMedia API');
-    }
-}
-
-function getUserMediaSuccess(stream) {
-    localStream = stream;
-    localVideo.src = window.URL.createObjectURL(stream);
 }
 
 function start(isCaller) {
